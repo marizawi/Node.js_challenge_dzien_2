c387f87773af12a8d687114cdfd2278ebdd7910f
Maria Zawiązalec <maria.zawiazalec@sagiton.pl>
2018-05-08 16:35:40 +0200

    day 2

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,22 @@
-//Twój kod
+const fs = require('fs');
+
+fs.readFile('./data/zadanie01/input.json', 'utf8', (err, data) => {
+    if (err === null) {
+        const numbers = JSON.parse(data);
+        let sum = 0;
+        for (let i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        fs.writeFile('./data/zadanie01/sum.txt', sum, err => {
+            if (err === null){
+                console.log('Zapisano poprawnie!');
+            } else {
+                console.log('Błąd podczas zapisu pliku!', err);
+            }
+        });
+
+    } else {
+        console.log('Błąd podczas odczytu pliku!', err);
+    }
+});
--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,24 @@
-//Twój kod
+const fs = require('fs');
+
+fs.readFile('./data/zadanieDnia/test.txt', 'utf8', (err, data) =>{
+    if (err === null) {
+        console.log('Poprawnie odczytano plik.', data);
+        let output = "";
+        for (let i = 0; i < data.length; i++) {
+            if (i % 2 === 0) {
+                output += data[i].toUpperCase();
+            } else {
+                output += data[i].toLowerCase();
+            }
+        }
+        fs.writeFile('./data/zadanieDnia/test.txt', output, err => {
+            if (err === null){
+                console.log('Zapisano poprawnie!');
+            } else {
+                console.log('Błąd podczas zapisu pliku!', err);
+            }
+        });
+    } else {
+        console.log('Błąd podczas odczytu pliku!', err);
+    }
+});
